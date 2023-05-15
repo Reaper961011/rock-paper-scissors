8261b21bb8694a37ce5e314798a3ea0143c17684
Tristan <tris080atala@gmail.com>
2023-05-15 10:09:18 +0200

    Completed JavaScript to accomplish objectives. Game is finally finished. Coming back later to add DOM work so that all the events happen on the main HTML.

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,65 @@
+let userChoice;
+let computerChoice;
+let winner;
+let computerWin = 0;
+let userWin = 0;
+
+
+
 function getComputerChoice(){
     let choices = Math.floor(Math.random() * 3 ) + 1;
     if (choices == 1){
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (choices == 2){
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else if(choices == 3){
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
-
     return computerChoice;
+};
+
+
+function playRound(userChoice, computersChoice){
+    if(userChoice == computersChoice){
+        computerWin++;
+        userWin++;
+    } else if (
+        (userChoice == "rock" && computersChoice == "scissors") || 
+        (userChoice == "scissors" && computersChoice == "papper") || 
+        (userChoice == "paper" && computersChoice == "rock")
+        ){
+        userWin++;
+    } else if (
+        (computersChoice == "rock" && userChoice == "scissors") || 
+        (computersChoice == "scissors" && userChoice == "papper") || 
+        (computersChoice == "paper" && userChoice == "rock")
+    ){
+        computerWin++;
+    }
 }
+
+function playGame(){
+    for (let i = 0; i < 5; i++) {
+        console.log(userChoice = prompt("Choose Rock, Paper or Scissors"));
+        userChoice = userChoice.toLowerCase();
+        console.log(getComputerChoice());
+        console.log(playRound(userChoice, computerChoice));
+        if (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors') {
+          console.log(userChoice = prompt("You spelled incorrectly, please try again."));
+        }
+      }
+    
+    if(userWin < computerWin){
+        winner = "The computer beat your sorry ass!"
+        return winner
+    } else if(userWin > computerWin){
+        winner = "You are the winner!"
+        return winner
+    } else if(userWin == computerWin){
+        winner = "It's a draw!"
+        return winner
+        
+    }
+}
+
+console.log(playGame())
